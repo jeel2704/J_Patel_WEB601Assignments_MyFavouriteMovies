30a061dd23389eb6cd73c0f50fe9e603207c2d35
jeel2704 <jp196@myscc.ca>
2023-03-03 17:18:33 -0500

    changed content_card.component.ts file

--- J_Patel_MyFavouriteMovies/src/app/content-card/content-card.component.ts
+++ J_Patel_MyFavouriteMovies/src/app/content-card/content-card.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
   styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent {
-
+  @Input() content: any;
 }
