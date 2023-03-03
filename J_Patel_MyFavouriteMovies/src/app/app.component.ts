e00d692d6b31070d6cbc91bad322e5d124c1167d
jeel2704 <jp196@myscc.ca>
2023-03-03 16:00:41 -0500

    added array of 6 items
    add an array of items in content-list
    added CSS and HTML

--- J_Patel_MyFavouriteMovies/src/app/app.component.ts
+++ J_Patel_MyFavouriteMovies/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ContentListComponent } from './content-list/content-list.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- J_Patel_MyFavouriteMovies/src/app/content-list/content-list.component.ts
+++ J_Patel_MyFavouriteMovies/src/app/content-list/content-list.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-content-list',
   templateUrl: './content-list.component.html',
   styleUrls: ['./content-list.component.scss']
 })
-export class ContentListComponent {
+export class ContentListComponent implements OnInit{
+  content = [    {
+    id: 1,
+    title: 'First Content',
+    description: 'This is the first content',
+    image: 'https://example.com/image1.jpg',
+    type: 'first',
+    tags: ['tag1', 'tag2', 'tag3'],
+    creator: 'Stan lee'
+  },
+    {
+      id: 2,
+      title: 'Second Content',
+      description: 'This is the second content',
+      image: 'https://example.com/image2.jpg',
+      type: 'second',
+      tags: ['tag4', 'tag5', 'tag6'],
+      creator: 'James Cameron'
+    },
+    {
+      id: 3,
+      title: 'Third Content',
+      description: 'This is the third content',
+      image: 'https://example.com/image3.jpg',
+      type: 'Third',
+      tags: ['tag7', 'tag8', 'tag9'],
+      creator: 'John Smith',
+    },
+    {
+      id: 4,
+      title: 'Forth Content',
+      description: 'This is the Forth content',
+      image: 'https://example.com/image1.jpg',
+      type: 'Forth',
+      tags: ['tag1', 'tag2', 'tag3'],
+      creator: 'Stan lee'
+    },
+    {
+      id: 5,
+      title: 'Fifth Content',
+      description: 'This is the Fifth content',
+      image: 'https://example.com/image1.jpg',
+      type: 'Fifth',
+      tags: ['tag1', 'tag2', 'tag3'],
+      creator: 'Stan lee'
+    },
+    {
+      id: 6,
+      title: 'sixth Content',
+      description: 'This is the sixth content',
+      image: 'https://example.com/image1.jpg',
+      type: 'sixth',
+      tags: ['tag1', 'tag2', 'tag3'],
+      creator: 'Stan lee'
+    },
+  ];
+  showIdAndTitle(id: number, title: string) {
+    console.log(`Content id: ${id}, title: ${title}`);
+  }
 
+  constructor() { }
+
+  ngOnInit() {
+  }
 }
